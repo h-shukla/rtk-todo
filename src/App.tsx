ba421c76502a1552a44cabd016f968890b1dbf91
Himanshu Shukla <hshukla2611@gmail.com>
2025-10-31 01:54:28 +0530

    mengafweaf

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 function App() {
     return (
-        <div className="flex flex-col gap-5 justify-center items-center min-h-[100vw]">
+        <div className="flex flex-col gap-5 justify-center items-center min-h-100">
             <h1 className="text-3xl font-bold italic underline">Hello world</h1>
             <Button>Click</Button>
         </div>
